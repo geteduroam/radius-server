f12fbbdd051a62c7dad53c9564fe6a34476cb328
Jørn Åne de Jong <jorn.dejong@uninett.no>
2020-11-11 11:18:06 +0100

    Add radsec support

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ A simple RADIUS server to get you started with geteduroam
 
 * FreeBSD 12 or higher
 * A CA to verify client certificates against
-* Server certificate with chain and corresponding key (may be signed by a different CA)
+* Server certificate with chain and corresponding key for RADIUS
+* Server certificate with chain and corresponding key for Radsec
 
 ## Instructions
 
-* Copy `ca.pem`, `server.key` and `server.pem` to `raddb/certs`
+* Copy RADIUS `ca.pem`, `server.key` and `server.pem` to `raddb/certs`
+* Copy Radsec `ca.pem`, `server.key` and `server.pem` to `radsecproxy/certs`
 * Run install.sh
--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ stat -l \
 	raddb/certs/server.pem >/dev/null \
 		|| { echo "==> Add key material to raddb/certs before running $0" >&2 ; false; }
 
-if [ -d /usr/local/etc/raddb ]
+if [ -d /usr/local/etc/raddb -o -f /usr/local/etc/radsecproxy.conf ]
 then
-	echo '==> WARNING: /usr/local/etc/raddb already exists'
+	[ -d /usr/local/etc/raddb ] && echo '==> WARNING: /usr/local/etc/raddb already exists'
+	[ -f /usr/local/etc/radsecproxy.conf ] && echo '==> WARNING: /usr/local/etc/radsecproxy.conf already exists'
 	echo '==> This script will destroy the existing RADIUS setup'
 	echo '==> Press CTRL+C within 10 seconds to safely abort'
 	printf ..........
@@ -24,43 +25,44 @@ then
 	echo '==> Safe abort time exceeded. Continuing installation'
 fi
 
-pkg install -y freeradius3
+pkg install -y freeradius3 radsecproxy
 
 rm -f \
-	/usr/local/etc/raddb/sites-enabled/default \
-	/usr/local/etc/raddb/sites-enabled/inner-tunnel \
-	/usr/local/etc/raddb/mods-enabled/eap \
-	/usr/local/etc/raddb/certs/ca.pem \
-	/usr/local/etc/raddb/certs/server.key \
-	/usr/local/etc/raddb/certs/server.pem \
+	/usr/local/etc/raddb/sites-enabled/default	\
+	/usr/local/etc/raddb/sites-enabled/inner-tunnel	\
+	/usr/local/etc/raddb/mods-enabled/eap	\
+	/usr/local/etc/raddb/certs/ca.pem	\
+	/usr/local/etc/raddb/certs/server.key	\
+	/usr/local/etc/raddb/certs/server.pem	\
 	\
-	/usr/local/etc/raddb/mods-enabled/attr_filter \
-	/usr/local/etc/raddb/mods-enabled/files \
-	/usr/local/etc/raddb/mods-enabled/preprocess \
-	/usr/local/etc/raddb/mods-enabled/realm \
-	/usr/local/etc/raddb/mods-enabled/cache_eap \
-	/usr/local/etc/raddb/mods-enabled/chap \
-	/usr/local/etc/raddb/mods-enabled/date \
-	/usr/local/etc/raddb/mods-enabled/detail \
-	/usr/local/etc/raddb/mods-enabled/digest \
-	/usr/local/etc/raddb/mods-enabled/dynamic_clients \
-	/usr/local/etc/raddb/mods-enabled/echo \
-	/usr/local/etc/raddb/mods-enabled/exec \
-	/usr/local/etc/raddb/mods-enabled/expiration \
-	/usr/local/etc/raddb/mods-enabled/expr \
-	/usr/local/etc/raddb/mods-enabled/linelog \
-	/usr/local/etc/raddb/mods-enabled/logintime \
-	/usr/local/etc/raddb/mods-enabled/mschap \
-	/usr/local/etc/raddb/mods-enabled/ntlm_auth \
-	/usr/local/etc/raddb/mods-enabled/pap \
-	/usr/local/etc/raddb/mods-enabled/passwd \
-	/usr/local/etc/raddb/mods-enabled/radutmp \
-	/usr/local/etc/raddb/mods-enabled/replicate \
-	/usr/local/etc/raddb/mods-enabled/soh \
-	/usr/local/etc/raddb/mods-enabled/sradutmp \
-	/usr/local/etc/raddb/mods-enabled/unix \
-	/usr/local/etc/raddb/mods-enabled/unpack \
-	/usr/local/etc/raddb/mods-enabled/utf8
+	/usr/local/etc/raddb/mods-enabled/attr_filter	\
+	/usr/local/etc/raddb/mods-enabled/files	\
+	/usr/local/etc/raddb/mods-enabled/preprocess	\
+	/usr/local/etc/raddb/mods-enabled/realm	\
+	/usr/local/etc/raddb/mods-enabled/cache_eap	\
+	/usr/local/etc/raddb/mods-enabled/chap	\
+	/usr/local/etc/raddb/mods-enabled/date	\
+	/usr/local/etc/raddb/mods-enabled/detail	\
+	/usr/local/etc/raddb/mods-enabled/digest	\
+	/usr/local/etc/raddb/mods-enabled/dynamic_clients	\
+	/usr/local/etc/raddb/mods-enabled/echo	\
+	/usr/local/etc/raddb/mods-enabled/exec	\
+	/usr/local/etc/raddb/mods-enabled/expiration	\
+	/usr/local/etc/raddb/mods-enabled/expr	\
+	/usr/local/etc/raddb/mods-enabled/linelog	\
+	/usr/local/etc/raddb/mods-enabled/logintime	\
+	/usr/local/etc/raddb/mods-enabled/mschap	\
+	/usr/local/etc/raddb/mods-enabled/ntlm_auth	\
+	/usr/local/etc/raddb/mods-enabled/pap	\
+	/usr/local/etc/raddb/mods-enabled/passwd	\
+	/usr/local/etc/raddb/mods-enabled/radutmp	\
+	/usr/local/etc/raddb/mods-enabled/replicate	\
+	/usr/local/etc/raddb/mods-enabled/soh	\
+	/usr/local/etc/raddb/mods-enabled/sradutmp	\
+	/usr/local/etc/raddb/mods-enabled/unix	\
+	/usr/local/etc/raddb/mods-enabled/unpack	\
+	/usr/local/etc/raddb/mods-enabled/utf8	\
+
 
 cp raddb/certs/ca.pem raddb/certs/server.key raddb/certs/server.pem /usr/local/etc/raddb/certs/
 cp raddb/clients.conf /usr/local/etc/raddb/clients.conf
@@ -71,3 +73,11 @@ cp raddb/sites-available/* /usr/local/etc/raddb/sites-enabled/
 sed -i -e '/^proxy_requests[[:space:]]\s/ s/yes/no/' /usr/local/etc/raddb/radiusd.conf
 
 sysrc radiusd_enable=YES
+
+cp radsecproxy.conf /usr/local/etc/
+mkdir -p /usr/local/etc/radsecproxy/certs/
+cp radsecproxy/certs/ca.pem radsecproxy/certs/server.key radsecproxy/certs/server.pem /usr/local/etc/radsecproxy/certs/
+sysrc radsecproxy_enable=YES
+
+service start radiusd
+service start radsecproxy
